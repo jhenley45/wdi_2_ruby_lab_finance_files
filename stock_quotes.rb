09e91abd7e1bfe6792942b7aad8dcbca904b1586
Jack Henley <jack.d.henley@gmail.com>
2014-01-15 14:25:03 -0500

    using ARGV for stock symbols

--- stock_quotes.rb
+++ stock_quotes.rb
@@ -2,3 +2,31 @@ require 'yahoo_finance'
 require 'pry'
 
 # Your Code Here
+
+
+# quote_type = YahooFinance::StandardQuote
+
+
+#puts "Please enter a stock ticker symbol."
+sym = ARGV[0]
+sym = sym.strip
+
+
+data = YahooFinance.quotes([sym], [:last_trade_price])
+price = data[0].last_trade_price
+
+puts "The current price for #{sym} is #{price}"
+
+puts "Your previous searches are:"
+read = File.open('quotes.csv')
+read.each do |line|
+	puts line
+end
+
+
+
+
+
+#writes current search to csv file
+file = File.open('quotes.csv', 'a')
+file.puts "#{sym}, #{price}, #{Time.now}"
